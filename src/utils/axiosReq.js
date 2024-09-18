aefb2e7df27e80c0a2dc6ba8f8f337134a9ddc51
hamim <hamim2114@gmail.com>
2024-09-18 10:31:43 +0600

    updated

--- src/utils/axiosReq.js
+++ src/utils/axiosReq.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const baseURL = window.location.hostname === 'localhost' 
   ? 'http://localhost:5000/api' 
-  : 'https://posh-coder-server.vercel.app/api';
+  : 'https://poshcoder-server.vercel.app/api';
 
 export const axiosReq = axios.create({
   baseURL: baseURL,
